a39680b3285d6884c7b11cf99c1d4aa6b916ccc5
Sweety <53006089+sweet1125@users.noreply.github.com>
2022-09-22 23:29:05 +0530

    3

---
+++ practical_no_3/Q1serial.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <time.h>
+
+#define n 100000
+void sort(int arr[])
+{
+    int i, j, first;
+    for (i = 0; i < n; i++)
+    {
+        first = i % 2;
+
+        for (j = first; j < n - 1; j += 2)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+void sort_des(int arr[])
+{
+    int i, j, first;
+    for (i = 0; i < n; ++i)
+    {
+        first = i % 2;
+
+        for (j = first; j < n - 1; j += 2)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int arr1[n], arr2[n];
+    int i;
+    for (i = 0; i < n; i++)
+    {
+
+        arr1[i] = n - i;
+    }
+    for (i = 0; i < n; i++)
+    {
+
+        arr2[i] = i;
+    }
+
+    clock_t t;
+    t = clock();
+
+    sort(arr1);
+    sort_des(arr2);
+
+    t = clock() - t;
+    double time_taken = ((double)t) / CLOCKS_PER_SEC;
+
+    printf("Time taken (seq): %f\n", time_taken);
+
+    int sum = 0;
+    for (i = 0; i < n; i++)
+    {
+
+        sum = sum + (arr1[i] * arr2[i]);
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
---
+++ practical_no_3/Q2matrixmul_seq.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#define N 2000
+
+void add(int **a, int **b, int **c)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+void getMatrix(int **a, int num)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            a[i][j] = num;
+        }
+    }
+}
+void display(int **a)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int **a;
+    int **b;
+    int **c;
+
+    a = malloc(sizeof(int *) * N);
+    b = malloc(sizeof(int *) * N);
+    c = malloc(sizeof(int *) * N);
+
+    for (int i = 0; i < N; i++)
+    {
+        a[i] = malloc(sizeof(int) * N);
+        b[i] = malloc(sizeof(int) * N);
+        c[i] = malloc(sizeof(int) * N);
+    }
+    getMatrix(a, 1);
+    getMatrix(b, 1);
+    double start;
+    double end;
+    start = omp_get_wtime();
+
+    add(a, b, c);
+    end = omp_get_wtime();
+
+    printf("Time taken (seq): %f\n", end - start);
+}
